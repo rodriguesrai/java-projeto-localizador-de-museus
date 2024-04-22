345329aa139fb7cf996588b39c2638c3e0b51e52
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-04-22 16:09:55 -0300

    req 7 - ControllerAdvice

---
+++ src/main/java/com/betrybe/museumfinder/advice/GeneralControllerAdvice.java
@@ -0,0 +1,30 @@
+package com.betrybe.museumfinder.advice;
+
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * The type General controller advice.
+ */
+@RestControllerAdvice
+public class GeneralControllerAdvice {
+
+  @ExceptionHandler(InvalidCoordinateException.class)
+  public ResponseEntity<String> handleInvalidCoordinateException(InvalidCoordinateException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Coordenada inválida!");
+  }
+
+  @ExceptionHandler(MuseumNotFoundException.class)
+  public ResponseEntity<String> handleMuseumNotFoundException(MuseumNotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Museu não encontrado!");
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleGenericException(Exception ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno!");
+  }
+}
